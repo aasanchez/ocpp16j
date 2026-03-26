4cc5903bdfe89542b29f3671cafd5d3222b52609
Alexis Sánchez <aasanchez@gmail.com>
2026-03-26 15:34:43 +0100

    CleanUp

--- tests_fuzz/doc.go
+++
@@ -1,2 +0,0 @@
-// Package tests_fuzz contains opt-in fuzz tests for the OCPP-J transport layer.
-package tests_fuzz
--- tests_race/doc.go
+++
@@ -1,2 +0,0 @@
-// Package ocpp16json_test contains race-detector-focused black-box tests.
-package ocpp16json_test
